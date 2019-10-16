44f80f463866cbb7629ebc9e8605b21e79bab27b
Rafael Dória <ieieldoria@hotmail.com>
2019-10-16 20:59:41 -0300

    Capturando dados para envio

--- src/routes/crawler.js
+++ src/routes/crawler.js
@@ -24,7 +24,38 @@ router.post('/', (req, res) => {
     let max_price = req.body.max_price
 
     let url = 'https://seminovos.com.br/' + type + '/' + mark + '/' + name + '/ano-' + init_year + '-' + end_year + '/preco-' + min_price + '-' + max_price
-    console.log(url)
+    
+    request(url, (err, response, body) => {
+        if (err || response.statusCode != 200) {
+            return;
+        }
+
+        let $ = cheerio.load(body)
+        let data = []
+
+        $('.card-content').each((key, element) => {
+            let elementTitle = $(element).find('.card-heading > .card-title').text()
+            let elementprice = $(element).find('.card-heading > .card-price').text()
+            let elementSubtitle = ($(element).find('.card-info > .card-features > .card-subtitle').text()).substring(9)
+            let elementYear = ($(element).find('.card-info > .card-features > .list-features > li[title|="Ano de fabricação"]').text()).substring(2)
+            let elementKm = (($(element).find('.card-info > .card-features > .list-features > li[title|="Kilometragem atual"]').text()).substring(2)).slice(0, -1)
+            let elementLink = ('https://seminovos.com.br/' + $(element).find('a').attr('href')).split('/')
+            elementLink = elementLink[0] + '/' + elementLink[2] + '/' + elementLink[4]
+
+            data.push({
+                elementTitle,
+                elementprice,
+                elementSubtitle,
+                elementYear,
+                elementKm,
+                elementLink
+            })
+        })
+
+        res.status(200).json(data)
+    })
+
+    
 })
 
 module.exports = router
